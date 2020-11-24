1f09eac6257ca2865add5e94e4c8e963c694d710
cmatsuta <cmatsuta@jbs.com>
2020-11-24 14:07:06 -0800

    modify server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const workout = require("./workoutModel.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", { useNewUrlParser: true });
+
+app.post("/submit", ({body}, res) => {
+  workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
